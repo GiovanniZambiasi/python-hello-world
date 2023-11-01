61cd5cb5b90e7681e7cd53348c23d90fdd48774e
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2023-11-01 11:29:18 -0300

    Add formatting examples

--- Strings.py
+++ Strings.py
@@ -1,5 +1,5 @@
+## Indexing
 helloWorld = "Hello World!"
-
 print(helloWorld)
 print(helloWorld[0])         # H
 print(helloWorld[0:-1:1])    # Slicing: Copy every character from 0 to -1 (last index, exclusive) with a step of one
@@ -8,6 +8,7 @@ print(helloWorld[0:5])       # Copy every char from 0 to 4
 print(helloWorld[::2])       # Copy every 2 chars from 0 till the end
 print(helloWorld[::-1])      # Cleverly reverses a string
 
+## Concatenation
 name = "Sam"
 # name[0] = 'P'         # Won't work, strings are immutable in python
 lastLetters = name[1:]
@@ -20,3 +21,14 @@ print(letter)
 
 print(helloWorld.split())   # Splits string using whitespace as the divider
 print(helloWorld.split('o')) # Splits string using o as the divider
+
+## Formatting
+print("Formatting {} is easy!".format("strings"))   
+print("The {} {} {}".format("quick", "brown", "fox"))               # By default, formats in order
+print("The {2} {1} {0}".format("fox", "brown", "quick"))            # Formatting by index is also supported
+print("The {q} {b} {f}".format(f="fox", b="brown", q="quick"))      # Finally, keywords can be used for formatting
+
+result = 100/777
+print("Result was {r:1.3f}".format(result))     # Formatting floats uses notation {value:width.precision f}
+name = "Jose"
+print(f"Hello, his name is {name}")     # Newer method of formatting works directly with the variable names. Add 'f' before string literal
