a01196a0fd7b77991d337899f28e66f22cf936cf
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2023-11-01 16:06:51 -0300

    Add logical and comparison operators

--- Bools.py
+++ Bools.py
@@ -1,2 +1,10 @@
 myBool = False      # False and True are capitalized
 print(type(True))   # Prints 'bool'
+
+myBool = 1 < 2 > 3  # Chaining comparison operators is allowed
+
+# 'and', 'or' and 'not' are logical operators
+myBool = 1 < 2 and 2 > 3 
+myBool = 1 == 2 or 'a' == 'a' 
+myBool = not 1==2
+myBool = not(10 < 0) and 3 > 2
