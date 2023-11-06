ab24ee2b5c53a8db554995b18656231852b196d4
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2023-11-06 18:14:22 -0300

    Implement cups

---
+++ Challenges/Cups.py
@@ -0,0 +1,29 @@
+from random import shuffle
+
+def player_guess():
+    guess = ""
+
+    while guess not in ["0", "1", "2"]:
+        guess = input("Pick a number (0-2)!")
+
+    return int(guess)
+
+def check_guess(cups, guess):
+    if cups[guess] == "O":
+        print("You win!")
+    else:
+        print("You lose :(")
+    
+    print(cups)
+
+
+cups = [" ", "O", " "]
+print("Welcome to CUPS!\nGuess in which cup the ball is")
+print(cups)
+input("Press enter to shuffle")
+print("Shuffling...")
+shuffle(cups)
+
+guess = player_guess()
+
+check_guess(cups, guess)
