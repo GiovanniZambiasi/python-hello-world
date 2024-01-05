7ae4838b65c6027672ddba3b1a12b85632afbd26
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2024-01-05 17:59:25 -0300

    Add more module things

--- Modules.py
+++ Modules.py
@@ -5,6 +5,15 @@ from SomeModule import some_func
 
 some_func()
 
+# An alternative way to call this function is as follows:
+import SomeModule
+SomeModule.some_func()
+
+# We can also import everything from a module using this syntax
+from SomeModule import *
+
+some_other_func()
+
 # Importing modules from a package works like this:
 from GiosPackage import GiosModuleMain
 
@@ -15,3 +24,15 @@ GiosModuleMain.report_main()
 from GiosPackage.GiosSubPackage import GiosSubModule
 
 GiosSubModule.sub_report()
+
+# __name__ is a built-in variable that stores the name of an object (in this case, the module itself). Usually, this holds the module name ('Modules', in this case)
+# however, when a python module is run directly (such as when running 'python Modules.py' from the cmd), the __name__ variable for this specific module gets overwritten
+# to "__main__". So, if we want to check if a particular module is being run directly (as opposted to being imported from some other module), we can perform the following check:
+if __name__ == "__main__":
+    print("I am being run directly!")
+else:
+    print("I am being imported from another module!")
+
+
+# Other things (such as modules, functions, classes also have a __name__)
+print(GiosModuleMain.__name__)
--- SomeModule.py
+++ SomeModule.py
@@ -1,2 +1,5 @@
 def some_func():
     print("Hey! This is from SomeModule")
+
+def some_other_func():
+    print("Hey! This is also from SomeModule")
